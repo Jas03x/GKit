a9f89ec33a79abf8259f9cf704606d5c5a99b4ee
Jas <jas03x@gmail.com>
2020-09-19 14:16:11 -0400

    Use 24 bit depth buffer

--- src/context.cpp
+++ src/context.cpp
@@ -48,6 +48,7 @@ bool Context::CreateInstance(const char* title, unsigned int width, unsigned int
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+	SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 
     if(status)
     {
