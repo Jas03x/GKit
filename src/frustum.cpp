e17f21146c9fd5743703c018dec91bc18e931f59
Jas <jas03x@gmail.com>
2020-11-22 02:21:17 -0500

    refractor frustum check function

--- src/frustum.cpp
+++ src/frustum.cpp
@@ -4,29 +4,18 @@ bool Frustum::InView(const AABB& aabb) const
 {
     bool in_view = true;
 
-#if 1
     for(unsigned int i = 0; i < COUNT; i++)
     {
         const Plane& p = planes[i];
         float radius = Vector::Length(aabb.radius);
 
         // sphere test
-        if(Vector::Dot(aabb.origin, p.n) + p.d <= -radius)
+        if(Vector::Dot(aabb.origin, p.n) + p.d + radius <= 0)
         {
             in_view = false;
             break;
         }
     }
-#else
-    const Plane& p = planes[NEAR];
-    float radius = Vector::Length(aabb.radius);
-
-    // sphere test
-    if(Vector::Dot(aabb.origin, p.n) + p.d < -radius)
-    {
-        in_view = false;
-    }
-#endif
 
     return in_view;
 }
